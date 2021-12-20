ae97edff59d986467bb6e7f0d763c974ac756d99
hspan <hspan000@gmail.com>
2021-12-20 16:51:05 +0900

    중복 삭제

--- stockchart.go
+++
@@ -1,9 +0,0 @@
-package creon
-
-type StockChart struct {
-	CpClass
-}
-
-func (c *StockChart) Create() {
-	c.CpClass.Create("CpSysDib.StockChart")
-}
--- stockmst.go
+++
@@ -1,9 +0,0 @@
-package creon
-
-type StockMst struct {
-	CpClass
-}
-
-func (c *StockMst) Create() {
-	c.CpClass.Create("DsCbo1.StockMst")
-}
